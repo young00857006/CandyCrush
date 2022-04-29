9ab8637009827f3b5d0e8fa1b60dbfd9a92b806d
T.Y.Kuan <tingyoungkuan@gmail.com>
2022-04-29 15:17:47 +0800

    Initial Commit

---
+++ Crush/CrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrushApp.swift
+//  Crush
+//
+//  Created by tingyang on 2022/4/29.
+//
+
+import SwiftUI
+
+@main
+struct CrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
